37946d6a90e02f5d71a7f5e5ff0aab61110dc576
chasechristensen1 <chase.christensen@inmoment.com>
2023-07-30 00:49:00 -0600

    fix

--- AI-Assistant/assistant-voice-google.py
+++ AI-Assistant/assistant-voice-google.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import subprocess
 import speech_recognition as sr
 
 def get_chat_response(prompt):
@@ -10,41 +11,38 @@ def get_chat_response(prompt):
     )
     return response.choices[0].text.strip()
 
+def recognize_speech():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        user_input = r.recognize_google(audio)
+        print(f"You: {user_input}")
+        return user_input
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+        return None
+    except sr.RequestError as e:
+        print(f"Could not request results from Speech Recognition service; {e}")
+        return None
+
 def say(text):
-    os.system(f"say {text}")
+    subprocess.run(["say", text])
 
 def main():
     print("Welcome to the Smart Assistant!")
-    # print("You can start by saying 'computer'.")
-
-    r = sr.Recognizer()
 
     while True:
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source)
-        
-        try:
-            user_input = r.recognize_google(audio)
-            print(f"You: {user_input}")
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-            continue
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
+        user_input = recognize_speech()
+        if user_input is None:
             continue
 
-        # if 'computer' not in user_input.lower():
-        #     print("Wake word not detected. Please start your request with 'computer'.")
-        #     continue
-
-        if user_input.lower() in ['exit', 'quit', 'bye']:
-            print("Smart Assistant: Goodbye!")
-            break
-        
         # Prepare the prompt for ChatGPT
         prompt = f"You: {user_input}\nSmart Assistant:"
-        
+
         # Get the response from ChatGPT
         response = get_chat_response(prompt)
 
@@ -53,7 +51,7 @@ def main():
 
         print(f"Smart Assistant: {response}")
 
-        # Convert the response to speech using macOS text-to-speech
+        # Use macOS text-to-speech for response
         say(response)
 
 if __name__ == "__main__":
